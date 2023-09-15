8a8c76dddcb3d4303c7061137851c2d17b34ffcb
unknown <patrialisakbargsi30a@gmail.com>
2023-09-15 12:03:54 +0700

    artikelubah

--- resources/views/pengunjung/artikel.blade.php
+++ resources/views/pengunjung/artikel.blade.php
@@ -5,8 +5,44 @@
 @section('content')
 <section id="blog">
   <div class="container mb-2 mt-5">
-    <h1>Artikel</h1>
-    
+  @foreach ($artikels as $Artikel)
+    <div class="row mb-2 mt-5">
+    <div class="font">
+    @foreach ($artikels as $Artikel)
+      <h2>Artikel</h2>
+    </div>
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative"> 
+        <div class="col p-4 d-flex flex-column position-static">
+        <h3 class="card-title">{{$Artikel->judul_artikel}}</h3>
+          <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+            Continue reading
+          </a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/></svg>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <h3 class="mb-0">Post title</h3>
+          <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+          Continue reading
+          </a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/></svg>
+        </div>
+      </div>
+
+     </div>
+    </div>
+    @endforeach
   </div>
   </section>
 @endsection
