c7a7890e7b9171823e5534dc43d3661a0fc02c6e
unknown <putrigsi30a@gmail.com>
2023-09-19 12:04:57 +0700

    terserahlah

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace Database\Seeders;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +18,19 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $user = [
+            [
+                'name' => 'putri',
+                'jabatan' => 'admin',
+                'password' => bcrypt('12345678'),
+                'email' => 'putri@gmail.com',
+                'gambar' => 'null'
+                
+            ],
+        ];
+        foreach ($user as $key => $value){
+            User::create($value);
+        }
     }
 }
